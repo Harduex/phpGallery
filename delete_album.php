af15618a9c3d4996388e8862d778351b212e9e7a
Harduex <simeon_simka@mail.bg>
2020-11-01 00:51:26 +0200

    more improvement of album visualisation

--- delete_album.php
+++ delete_album.php
@@ -2,4 +2,20 @@
 $album = basename($_GET['album']);
 
 header("Location: ./dashboard.php");
-rmdir('./albums/'.$album);
+function rrmdir($dir)
+{
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (filetype($dir . "/" . $object) == "dir")
+                    rrmdir($dir . "/" . $object);
+                else unlink($dir . "/" . $object);
+            }
+        }
+        reset($objects);
+        rmdir($dir);
+    }
+}
+
+rrmdir('./albums/' . $album);
--- index.php
+++ index.php
@@ -1,9 +1,11 @@
+<!DOCTYPE html>
 <html>
 
 <head>
     <link rel='stylesheet' href='css/style.css' />
-    <link rel='stylesheet' href='css/lightbox.min.css' />
-    <script src="js/lightbox-plus-jquery.min.js"></script>
+    <link rel='stylesheet' href='css/album-style.css' />
+    <meta charset="UTF-8" />
+    <title>Gallery</title>
 </head>
 
 <body>
@@ -13,19 +15,32 @@
         <h1>Browse Albums</h1>
         <?php
         $directory = dir('./albums');
-        echo "<ul>";
+
         while (($folder = $directory->read()) !== false) {
             if ($folder == "." || $folder == "..") continue;
-            echo "<li>";
 
-            echo "<a href='./view_album.php?album=$folder'>" . $folder . "</a>";
+            $album = './albums/' . $folder;
 
-            echo "</li>";
+            if (!(count(glob("$album/*")) === 0)) {
+                $img = random_pic($album);
+                echo '<div class="album-container">';
+                echo "<a href='./view_album.php?album=$folder'>
+                    <img src='$img' style='object-fit: cover;width:230px;height:230px;' alt='$folder' class='album-image'>
+                    </a>";
+                echo '<div class="overlay">' . $folder . '</div>';
+                echo '</div>';
+            }
         }
-        echo "</ul>";
-        ?>
-    </div>
 
+        function random_pic($dir)
+        {
+            $files = glob($dir . '/*.*');
+            $file = array_rand($files);
+            return $files[$file];
+        }
+        ?>
+
+    </div>
 
 </body>
 
